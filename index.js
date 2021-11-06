cbefeae68bcad117c81e9b4c02c5b9b31e677582
monkeyman9920 <80383629+monkeyman9920@users.noreply.github.com>
2021-11-06 15:15:11 -0500

    this only works for 12.5.3 of discord.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`${client.user.tag) is online`)
+  )}
+  
+  clien.login('token')
