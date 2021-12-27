8acddda644108ee75f46e218c96d31a5bdd379c3
monkeyman9920 <80383629+monkeyman9920@users.noreply.github.com>
2021-12-27 00:15:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client } = require('discord.js')
+const client = new Client({ intents: 32767 })
 
 client.on('ready', () => {
   console.log(`${client.user.tag) is online`)
   )}
   
   client.login('token')
-  
-///you need discord.js 12.5.3 and nodejs for this to work
