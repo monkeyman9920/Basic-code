1c105ad6cd4eee1b3457feeeaf5ec306cb73f3ec
monkeyman9920 <80383629+monkeyman9920@users.noreply.github.com>
2022-04-17 22:28:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = require('discord.js')
 const client = new Client({ intents: 32767 })
 
 client.on('ready', () => {
-  console.log(`${client.user.tag) is online`)
-  )}
-  
-  client.login('token')
+    console.log(`${client.user.tag} is online`)
+})
+
+client.login('token')
